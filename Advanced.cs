228ee439bf75f574b7500cf88529dfe00ca527e6
Ryan McBeth <ryan.mcbeth@gmail.com>
2021-05-04 18:57:04 -0400

    First Push
    Push for Exception Handling Tutorial

---
+++ Advanced.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using TryCatchFinallyThrowTutorial;
+using static ExceptionTutorial.TheBasics;
+
+namespace MathTesting
+{
+    public class Advanced
+    {
+
+  
+        ///Can you have more than one catch?
+        public static (ErrorMessages,int) DivideWithMultipleCatches(int Numerator, int Denominator)
+        {
+
+            int answer = 0;
+            ErrorMessages em;
+            try
+            {
+                CreateConnection();
+                answer = (Numerator / Denominator);
+                PushResultToDatabase(answer);
+                em = ErrorMessages.OK;
+            }
+            catch (DivideByZeroException e)
+            {
+                LogTransactions();
+                //Display it
+                Console.WriteLine(e.Message.ToString());
+                em = ErrorMessages.DenominatorCantBeZero;
+            }
+            catch (HttpResponseException e)
+            {
+                LogTransactions();
+                //Display it
+                Console.WriteLine(e.Message.ToString());
+                em = ErrorMessages.HttpConnectionError;
+            }
+            catch (Exception e)
+            {
+                LogTransactions();
+                //Display it
+                Console.WriteLine(e.Message.ToString());
+                em = ErrorMessages.GeneralException;
+            }
+            finally
+            {
+                TerminateConnection();
+            }
+            return (em, answer);
+
+
+        }
+
+
+        public static int DivideWithFinally(int Numerator, int Denominator)
+        {
+            int answer = 0;
+            try
+            {
+                CreateConnection();
+                answer = (Numerator / Denominator);
+                PushResultToDatabase(answer);
+            }
+            finally
+            {
+                TerminateConnection();
+            }
+
+            /*
+            using (CreateConnection())
+            {
+                answer = (Numerator / Denominator);
+                PushResultToDatabase(answer);
+            }
+            */
+
+            return answer;
+        }
+
+        public static int DontDoThis(int Numerator, int Denominator)
+        {
+            int answer = 0;
+            try
+            {
+                answer = (Numerator / Denominator);
+            }
+            catch
+            {
+
+            }
+            return answer;
+
+        }
+
+    }
+}
---
+++ TheBasics.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionTutorial
+{
+    public class TheBasics
+    {
+
+        public enum ErrorMessages
+        {
+            DenominatorCantBeZero,
+            NumeratorNotANumber,
+            DemoninatorNotANumber,
+            HttpConnectionError,
+            GeneralException,
+            OK
+
+        }
+
+        public static int DivideTwoNumbers(int numerator, int denominator)
+        {
+
+            return (numerator / denominator);
+
+        }
+
+        public static int DivideTwoNumbersWithHandling(int numerator, int denominator)
+        {
+            try
+            {
+
+                return (numerator / denominator);
+            }
+            catch (Exception e)
+            {
+                LogTransactions();
+                //Display it
+                Console.WriteLine(e.Message.ToString());
+
+            }
+
+
+
+
+
+            return 0;
+        }
+
+        public static (ErrorMessages, int) DivideTwoNumbersWithHandlingFinally(int numerator, int denominator)
+        {
+            int answer = 0;
+            ErrorMessages em;
+            try
+            {
+                CreateConnection();
+                answer = (numerator / denominator);
+                PushResultToDatabase(answer);
+                em = ErrorMessages.OK;
+            }
+            catch (Exception e)
+            {
+                LogTransactions();
+                //Display it
+                Console.WriteLine(e.Message.ToString());
+                em = ErrorMessages.DenominatorCantBeZero;
+            }
+            finally
+            {
+                TerminateConnection();
+            }
+            return (em,answer);
+            
+        }
+
+        public static (ErrorMessages,int) ValidateNumbersAndDivide(int Numerator, int Denominator)
+        {
+
+            if (Denominator == 0)
+            {
+                return (ErrorMessages.DenominatorCantBeZero, 0);
+            }
+            else
+            {
+                return (ErrorMessages.OK, DivideTwoNumbersWithHandling(Numerator, Denominator));
+            }
+
+        }
+
+        public static int CanYouDoThis(int Numerator, int Denominator)
+        {
+            int answer = 0;
+            
+            try
+            {
+                   CreateConnection();
+                   answer = (Numerator / Denominator);
+            }
+            catch
+            {
+                return 1;
+            }
+            finally
+            {
+                TerminateConnection();
+            }
+            return answer;
+
+        }
+
+        /// <summary>
+        /// Fake create connection
+        /// </summary>
+        public static void CreateConnection() 
+        {
+
+        }
+
+        /// <summary>
+        /// Fake Terminate Connection
+        /// </summary>
+        public static void TerminateConnection()
+        {
+
+        }
+
+        //Fake Logging
+        public static void LogTransactions()
+        {
+
+        }
+
+        //Fake Database Connection
+        public static void PushResultToDatabase(int Result)
+        {
+
+        }
+
+    }
+}
